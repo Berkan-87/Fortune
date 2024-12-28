945111de66e16578f823dd5a70c8584c4e713aeb
berkanyilmaz <sby.notess@gmail.com>
2024-12-28 22:33:51 +0300

    hatalar düzeltildi

--- js/task-2.js
+++ js/task-2.js
@@ -3,7 +3,7 @@ function getShippingMessage(country, price, deliveryFee) {
   return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
 
-console.log(getShippingMessage("Australia", 50, 10));
-console.log(getShippingMessage("Germany", 100, 20));
-console.log(getShippingMessage("Sweden", 200, 15));
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20));
+console.log(getShippingMessage("Sweden", 100, 20));
 
